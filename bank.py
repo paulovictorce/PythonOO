a458f9db1477d07b61a4462d3190ed27192c6d5f
paulohome <bd.paulovictor@gmail.com>
2018-01-11 16:47:35 -0300

    Implementando modificadores de aceso

--- bank.py
+++ bank.py
@@ -2,13 +2,13 @@ class Account:
 
     def __init__(self, number):
         self.number = number
-        self.total = 0
+        self.__total = 0
 
     def deposit(self, value):
-        self.total += value
+        self.__total += value
 
     def withdraw(self, value):
-        self.total -= value
+        self.__total -= value
 
     def getTotal(self):
-        return self.total
+        return self.__total
